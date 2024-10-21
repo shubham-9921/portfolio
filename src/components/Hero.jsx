f9b51966f6e615a468f9e76863d9105648b51408
Shubham Bodkhe <bodkheshubham9921@gmail.com>
2024-10-21 17:59:39 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,55 @@
+import profile from "../assets/profilepic7.png";
+
+import { TypeAnimation } from "react-type-animation";
+const Hero = () => {
+  return (
+    <div>
+      <div className="grid grid-cols-1  sm:grid-cols-3 gap-8 max-w-[1200px] md:h-[70vh] mx-auto py-8 bg-black">
+        <div className="col-span-1 my-auto mx-auto  w-[300px] h-auto  lg:w-[400px]">
+          <img src={profile} alt="Profile" />
+        </div>
+
+        <div className="col-span-2 px-5">
+          <h1 className="text-white text-4xl sm:text-5xl lg:text-8xl font-extrabold ">
+            <span className="primary-color"> I&apos;m a</span> <br />
+            <TypeAnimation
+              sequence={[
+                "Frontend Dev",
+                1000,
+                "Web Dev",
+                1000,
+                "Full Stack Dev",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+
+          <p className="text-white sm:text-lg lg:text-xl">
+            My name is Shubham Bodkhe and I have 1+ year of experience in web
+            development.
+          </p>
+
+          <div className="my-8">
+            <a
+              href="/"
+              className="px-6 py-3 w-full rounded-xl mr-4 bg-gradient-to-br from-orange-400 to bg-pink-500 text-white"
+            >
+              Download CV{" "}
+            </a>
+            <a
+              href="#contact"
+              className="px-6 py-3 w-full rounded-xl mr-4 border border-gray-400   hover:border-none hover:bg-gradient-to-br from-orange-400 to bg-pink-500 text-white"
+            >
+              Contact
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <>
+      <div>
+        <div className="bg-black text-gray-400 h-[100px] mx-auto max-w-[1200px] flex justify-between items-center">
+          <h1 className="text-3xl font-bold  primary-color ml-4 ">S.B</h1>
+          <ul className=" hidden md:flex">
+            <li className="p-5">
+              <a href="#about">About</a>
+            </li>
+            <li className="p-5">
+              <a href="#work">Work</a>
+            </li>
+            <li className="p-5">
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+          <div className="block md:hidden mr-6" onClick={handleNav}>
+            {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+          </div>
+
+          <div
+            className={
+              nav
+                ? `fixed h-full left-0 top-0 width-[60%] bg-[#202121] ease-in-out duration-500 `
+                : `fixed left-[-100%]`
+            }
+          >
+            <h1 className="text-3xl font-bold m-4 primary-color">S.B</h1>
+            <ul className="p-8 text-2xl">
+              <li className="p-2">
+                <a href="#home">Home</a>
+              </li>
+              <li className="p-2">
+                <a href="#about">About</a>
+              </li>
+              <li className="p-2">
+                <a href="#work">Work</a>
+              </li>
+              <li className="p-2">
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
